b30e0bda02ac2b697b8c24b5f23cb0cd3178dbff
ikhsan fajar <ikhsanfajar17@gmail.com>
2023-06-21 14:25:46 +0000

    cicd

--- config.php
+++ config.php
@@ -1,10 +1,9 @@
 <?php
 
-$db_host = getenv("DB_HOST");
-$db_user = getenv("DB_USER");
-$db_pass = getenv("DB_PASS");
-$db_name = getenv("DB_NAME");
-
+$db_host = "db";
+$db_user = "devopscilsy";
+$db_pass = "1234567890";
+$db_name = "dbsosmed";
 
 try {    
     //create PDO connection 
